e064412a170089772a9f4f621b8128dba976e538
Stephen Grey <Stephen.Grey@thomsonreuters.com>
2018-02-13 06:23:52 +0000

    ownsearch stage1

--- wwwsearch/ownsearch/solrJson.py
+++ wwwsearch/ownsearch/solrJson.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
 #from bs4 import BeautifulSoup as BS
 import requests, requests.exceptions
 import os, logging
